d192fde30d7a50422e2eb879adf609a365b00dff
diagonalc <diagonalchen@outlook.com>
2026-07-09 12:39:43 +0800

    done?

--- my_code/ch11/hostinfo.c
+++ my_code/ch11/hostinfo.c
@@ -1,18 +1,28 @@
 #include "csapp.h"
 
-int main(int argc, char **argv){
-    if(argc != 2){
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
         unix_error("format: ./hostinfo <website>");
     }
 
     struct addrinfo hints;
     struct addrinfo *result, *cur;
+    char host[MAXLINE];
+    char service[MAXLINE];
 
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
-    getaddrinfo(argv[1], NULL, &hints, &result);
-    for (cur = *result; cur; cur = cur -> ai_next){
-        getnameinfo
+    getaddrinfo(argv[1], 443, &hints, &result);
+
+    int flag = NI_NUMERICHOST;
+    for (cur = result; cur; cur = cur->ai_next)
+    {
+        getnameinfo(cur->ai_addr, cur->ai_addrlen, host, MAXLINE, service, MAXLINE, flag);
+        printf("host: %s\nservice: %s\n", host, service);
     }
+    freeaddrinfo(result);
+    return 0;
 }
