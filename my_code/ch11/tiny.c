d00ce3ed7cae489787445483c51ade14c52a8439
diagonalc <diagonalchen@outlook.com>
2026-07-22 00:50:20 +0800

    save

--- my_code/ch11/tiny.c
+++ my_code/ch11/tiny.c
@@ -56,10 +56,46 @@ void doit(int fd)
     }
 }
 
-
-void clienterror(int fd, char* cause, char* errnum, char* shortmsg, char* longmsg){
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg)
+{
     char buf[MAXLINE], body[MAXLINE];
-    sprintf()
+    //sprintf with a offset "len", as strcat does not accept placeholders like "%s" while something like "sprintf(buf, "%s %s", buf, uri)" is undefined
+    int len = 0;
+    len += sprintf(body + len, "<html><title>Tiny Error</title>");
+    len += sprintf(body + len, "<body bgcolor=\"ffffff\">\r\n");
+    len += sprintf(body + len, "%s: %s\r\n", errnum, shortmsg);
+    len += sprintf(body + len, "<p>%s: %s\r\n", longmsg, cause);
+    len += sprintf(body + len, "<hr><em>The Tiny Web server</em>\r\n");
+
+    sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+    Rio_writen(fd, buf, strlen(buf));
+    //although the buffer zone won't be flushed, sprintf will attach a \0 at the end and strlen(buf) will only count to \0
+    sprintf(buf, "Content-type: text/html\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+    Rio_writen(fd, buf, strlen(buf));
+    Rio_writen(fd, body, strlen(body));
+}
+
+void read_requesthdrs(rio_t *rp){
+    char buf[MAXLINE];
+
+    rio_readlineb(rio, buf, MAXLINE)
+    
+    while(strcmp(buf, "\r\n")){
+        printf("%s", buf);
+        rio_readlineb(rio, buf, MAXLINE);
+    }
+    return;
+}
+
+void parse_uri(char* uri, char *filename, char* cgiargs){
+    char *ptr;
+    if(!strstr(uri, "cgi-bin")){    //static content, as all dynamic content should be inside the folder "cgi-bin"
+        strcpy(cgiargs, "");        //erase cgiargs, as client requests a static content
+        strcpy(filename, ".");
+        strcpy
+    }
 }
 
 int main(int argc, char **argv)
